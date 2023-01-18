74d485df31b7fe76c078164c92d43e55f831ad6b
Chetan Singh <boyzingsing@gmail.com>
2023-01-18 15:06:05 +0530

    solution1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,31 +4,54 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
-    private String bill;
+    private String bill ;
+
+    private boolean cheese = false;
+    private boolean topping = false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg) {
+            this.price = 300;
+        }
+        else {
+            this.price = 400;
+        }
+        this.bill = "Base Price Of The Pizza: " + price + "\n";
     }
 
     public int getPrice(){
+        System.out.print("Total Price: ");
         return this.price;
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(this.cheese) return;
+        cheese = true;
+        this.price += 80;
+        this.bill += "Extra Cheese Added: 80\n";
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(this.topping) return;
+        topping = true;
+        if(isVeg) {
+            this.price += 70;
+            this.bill += "Extra Toppings Added: 70\n";
+        }
+        else {
+            this.price += 120;
+            this.bill += "Extra Toppings Added: 120\n";
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        this.price += 20;
+        this.bill += "Paperbag Added: 20\n";
     }
 
     public String getBill(){
-        // your code goes here
+        this.bill += "Total Price: " + price + "\n";
         return this.bill;
     }
 }
